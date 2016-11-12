7010e4a124fef55eba5a09fcdab9a3ad08651ab0
AndressaPilar <andressa.karla7@outlook.com>
2016-11-12 14:32:40 -0200

    Commits Iniciais Projeto
    Camada Web - - Iniado projeto novamente devido entrada novo membro na
    equipe

---
+++ ProjetoArtCouro/ProjetoArtCouro.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetoArtCouro.Web.Startup))]
+namespace ProjetoArtCouro.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
