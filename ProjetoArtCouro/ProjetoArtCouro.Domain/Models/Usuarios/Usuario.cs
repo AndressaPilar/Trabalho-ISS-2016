dbb71c1a05a05bbc7a1937e057895374ad969ef1
AndressaPilar <andressa.karla7@outlook.com>
2016-11-14 17:40:15 -0200

    Classe Usuário - ainda falta resolver as dependências

---
+++ ProjetoArtCouro/ProjetoArtCouro.Domain/Models/Usuarios/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Domain.Models.Usuarios
+{
+    public class Usuario
+    {
+        public Guid UsuarioId { get; set; }
+        public int UsuarioCodigo { get; set; }
+        public string UsuarioNome { get; set; }
+        public string Senha { get; set; }
+        public bool Ativo { get; set; }
+        public virtual Venda Venda { get; set; }
+        public virtual Compra Compra { get; set; }
+        public virtual GrupoPermissao GrupoPermissao { get; set; }
+        public virtual ICollection<Permissao> Permissoes { get; set; }
+
+        public void Validar()
+        {
+            AssertionConcern.AssertArgumentLength(UsuarioNome, 3, 250, Erros.InvalidUserName);
+            PasswordAssertionConcern.AssertIsValid(Senha);
+        }
+    }
+}
