ad16a3ca1c2c779fa5a6bc60a3fdc405111c6f11
TadaoHara <tadaohara@msn.com>
2016-11-14 17:54:39 -0200

    pessoa juridica e papel

---
+++ ProjetoArtCouro/ProjetoArtCouro.Domain/Models/Pessoas/PessoaJuridica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Domain.Models.Pessoas
+{
+    public class PessoaJuridica
+    {
+        public Guid PessoaId { get; set; }
+        public int PessoaJuridicaCodigo { get; set; }
+        public string CNPJ { get; set; }
+        public string Contato { get; set; }
+        public virtual Pessoa Pessoa { get; set; }
+
+        public void Validar()
+        {
+            AssertionConcern.AssertArgumentNotEmpty(CNPJ, Erros.EmptyCNPJ);
+            AssertionConcern.AssertArgumentNotEmpty(Contato, Erros.EmptyContact);
+        }
+    }
+}
