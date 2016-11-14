1844786ee9e04030fc1021da6ad7d7f4f6215196
GuGB <gustavo_gravena@hotmail.com>
2016-11-14 18:11:32 -0200

    MeioComunicacao - falta dependencias

---
+++ ProjetoArtCouro/ProjetoArtCouro.Domain/Models/Pessoas/MeioComunicacao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Domain.Models.Pessoas
+{
+    public class MeioComunicacao
+    {
+        public Guid MeioComunicacaoId { get; set; }
+        public int MeioComunicacaoCodigo { get; set; }
+        public string MeioComunicacaoNome { get; set; }
+        public TipoComunicacaoEnum TipoComunicacao { get; set; }
+        public bool Principal { get; set; }
+        public Guid PessoaId { get; set; }
+        public virtual Pessoa Pessoa { get; set; }
+    }
+}
