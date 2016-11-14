1f18c4cc146c397562a14d715ab50778bdccd7a1
GuGB <gustavo_gravena@hotmail.com>
2016-11-14 17:49:57 -0200

    Configurações Iniciais de Pessoa
    também falta resolver as dependências

---
+++ ProjetoArtCouro/ProjetoArtCouro.Domain/Models/Pessoas/Pessoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Domain.Models.Pessoas
+{
+    public class Pessoa
+    {
+        public Guid PessoaId { get; set; }
+        public int PessoaCodigo { get; set; }
+        public string Nome { get; set; }
+        public virtual PessoaFisica PessoaFisica { get; set; }
+        public virtual PessoaJuridica PessoaJuridica { get; set; }
+        public virtual Venda Venda { get; set; }
+        public virtual Compra Compra { get; set; }
+        public virtual Estoque Estoque { get; set; }
+        public virtual ICollection<Papel> Papeis { get; set; }
+        public virtual ICollection<MeioComunicacao> MeiosComunicacao { get; set; }
+        public virtual ICollection<Endereco> Enderecos { get; set; }
+
+        public void Validar()
+        {
+            AssertionConcern.AssertArgumentNotEmpty(Nome, Erros.EmptyName);
+            AssertionConcern.AssertArgumentNotNull(Papeis, Erros.PaperEmptyPerson);
+            AssertionConcern.AssertArgumentNotNull(MeiosComunicacao, Erros.MeansOfCommunicationEmpty);
+            AssertionConcern.AssertArgumentNotNull(Enderecos, Erros.EmptyAddress);
+        }
+    }
+}
