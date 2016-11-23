2cbe29756204a2264769893aaa345bfa2251cd6f
GuGB <gustavo_gravena@hotmail.com>
2016-11-23 19:00:07 -0200

    ContaReceber

---
+++ ProjetoArtCouro/ProjetoArtCouro.Domain/Models/Vendas/ContaReceber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Domain.Models.Vendas
+{
+    public class ContaReceber
+    {
+        public Guid ContaReceberId { get; set; }
+        public int ContaReceberCodigo { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public decimal ValorDocumento { get; set; }
+        public StatusContaReceberEnum StatusContaReceber { get; set; }
+        public bool Recebido { get; set; }
+        public virtual Venda Venda { get; set; }
+
+        public void Validar()
+        {
+            AssertionConcern.AssertArgumentNotEquals(new DateTime(), DataVencimento,
+                string.Format(Erros.InvalidParameter, "DataVencimento"));
+            AssertionConcern.AssertArgumentNotEquals(0.0M, ValorDocumento,
+                string.Format(Erros.NotZeroParameter, "ValorDocumento"));
+            AssertionConcern.AssertArgumentNotEquals(StatusContaReceber, StatusContaReceberEnum.None,
+                string.Format(Erros.InvalidParameter, "StatusContaReceber"));
+            AssertionConcern.AssertArgumentNotNull(Venda, Erros.SaleNotSet);
+        }
+    }
+}
