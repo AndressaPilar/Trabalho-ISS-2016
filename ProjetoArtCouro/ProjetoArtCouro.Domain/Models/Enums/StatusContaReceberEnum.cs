069beec352ace58dd13ca9b1c9ce1ec2ab3ac37c
GuGB <gustavo_gravena@hotmail.com>
2016-11-23 19:05:52 -0200

    Enum ContaReceber

---
+++ ProjetoArtCouro/ProjetoArtCouro.Domain/Models/Enums/StatusContaReceberEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Domain.Models.Enums
+{
+    public enum StatusContaReceberEnum
+    {
+        None = 0,
+        Aberto = 1,
+        Recebido = 2
+    }
+}
