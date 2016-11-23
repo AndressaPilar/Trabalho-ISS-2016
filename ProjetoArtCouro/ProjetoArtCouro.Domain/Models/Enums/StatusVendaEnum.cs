aab92b617bf4ded5558681289bdac7a18ad19cf2
TadaoHara <tadaohara@msn.com>
2016-11-23 19:10:20 -0200

    enuns StatusVenda e TipopapelPessoa

---
+++ ProjetoArtCouro/ProjetoArtCouro.Domain/Models/Enums/StatusVendaEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Domain.Models.Enums
+{
+    public enum StatusVendaEnum
+    {
+        None = 0,
+        Aberto = 1,
+        Confirmado = 2,
+        Cancelado = 3
+    }
+}
---
+++ ProjetoArtCouro/ProjetoArtCouro.Domain/Models/Enums/TipoPapelPessoaEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Domain.Models.Enums
+{
+    public enum TipoPapelPessoaEnum
+    {
+        Nenhum = 0,
+        PessoaFisica = 1,
+        PessoaJuridica = 2,
+        Funcionario = 3,
+        Cliente = 4,
+        Fornecedor = 5
+    }
+}
