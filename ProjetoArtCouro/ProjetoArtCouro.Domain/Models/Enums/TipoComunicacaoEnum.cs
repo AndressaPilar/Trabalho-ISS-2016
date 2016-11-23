e1e5117d94e1507c8a474f47d5f0d9af37ee6142
GuGB <gustavo_gravena@hotmail.com>
2016-11-23 19:31:25 -0200

    TipoComunicacaoEnum

---
+++ ProjetoArtCouro/ProjetoArtCouro.Domain/Models/Enums/TipoComunicacaoEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Domain.Models.Enums
+{
+    public enum TipoComunicacaoEnum
+    {
+        None = 0,
+        Telefone = 1,
+        Celular = 2,
+        Email = 3
+    }
+}
