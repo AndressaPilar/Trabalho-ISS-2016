a6156193679f6eb00bb4f265f2daf290a413e8e2
AndressaPilar <andressa.karla7@outlook.com>
2016-11-30 20:47:58 -0200

    Repositorio IService Usuario e Conta a Pagar

---
+++ ProjetoArtCouro/ProjetoArtCouro.Domain/Contracts/IService/ICompra/IContaPagarService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Domain.Contracts.IService.ICompra
+{
+    public interface IContaPagarService : IDisposable
+    {
+        List<ContaPagar> PesquisarContaPagar(int codigoCompra, int codigoFornecedor, DateTime dataEmissao,
+            DateTime dataVencimento, int statusContaPagar, string nomeFornecedor, string documento, int codigoUsuario);
+        void PagarContas(List<ContaPagar> contasPagar);
+    }
+}
