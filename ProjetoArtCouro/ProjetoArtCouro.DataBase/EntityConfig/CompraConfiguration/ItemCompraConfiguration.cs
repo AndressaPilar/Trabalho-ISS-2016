ea38ba367fe02f990af3decacd2df24cd7167bf7
shDiego <dschiavinatti@gmail.com>
2016-11-30 21:07:32 -0200

    item Compra configuração

---
+++ ProjetoArtCouro/ProjetoArtCouro.DataBase/EntityConfig/CompraConfiguration/ItemCompraConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.DataBase.EntityConfig.CompraConfiguration
+{
+    public class ItemCompraConfiguration : EntityTypeConfiguration<ItemCompra>
+    {
+        public ItemCompraConfiguration()
+        {
+            ToTable("ItemCompra");
+
+            Property(x => x.ItemCompraId)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(x => x.ItemCompraCodigo)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(x => x.ProdutoCodigo)
+                .IsRequired()
+                .HasColumnType("int");
+
+            Property(x => x.ProdutoNome)
+                .IsRequired()
+                .HasColumnType("varchar");
+
+            Property(x => x.Quantidade)
+                .IsRequired()
+                .HasColumnType("int");
+
+            Property(x => x.PrecoVenda)
+                .IsRequired()
+                .HasColumnType("decimal");
+
+            Property(x => x.ValorBruto)
+                .IsRequired()
+                .HasColumnType("decimal");
+
+            Property(x => x.ValorLiquido)
+                .IsRequired()
+                .HasColumnType("decimal");
+        }
+    }
+}
