2607b5464d07f9a815aa148883eb4f501f895349
AndressaPilar <andressa.karla7@outlook.com>
2016-11-30 20:55:13 -0200

    Service de FormaPagamento, Autenticacao e Usuario

---
+++ ProjetoArtCouro/ProjetoArtCouro.Domain/Contracts/IService/IAutenticacao/IAutenticacao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Domain.Contracts.IService.IAutenticacao
+{
+    public interface IAutenticacao : IDisposable
+    {
+        Usuario AutenticarUsuario(string usuarioNome, string senha);
+    }
+}
---
+++ ProjetoArtCouro/ProjetoArtCouro.Domain/Contracts/IService/IPagamento/IFormaPagamentoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Domain.Contracts.IService.IPagamento
+{
+    public interface IFormaPagamentoService : IDisposable
+    {
+        List<FormaPagamento> ObterListaFormaPagamento();
+        FormaPagamento ObterFormaPagamentoPorCodigo(int codigo);
+        FormaPagamento CriarFormaPagamento(FormaPagamento formaPagamento);
+        FormaPagamento AtualizarFormaPagamento(FormaPagamento formaPagamento);
+        void ExcluirFormaPagamento(int formaPagamentoCodigo);
+    }
+}
