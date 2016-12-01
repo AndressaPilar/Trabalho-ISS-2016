44259e76a6ce16d173ffc933126fd56e79ad7fc3
TadaoHara <tadaohara@msn.com>
2016-12-01 20:49:52 -0200

    servico Iproduto

---
+++ ProjetoArtCouro/ProjetoArtCouro.Domain/Contracts/IService/IProduto/IProdutoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Domain.Contracts.IService.IProduto
+{
+    public interface IProdutoService : IDisposable
+    {
+        List<Produto> ObterListaProduto();
+        List<Unidade> ObterListaUnidade();
+        Produto ObterProdutoPorCodigo(int codigo);
+        Produto CriarProduto(Produto produto);
+        Produto AtualizarProduto(Produto produto);
+        void ExcluirProduto(int produtoCodigo);
+    }
+}
