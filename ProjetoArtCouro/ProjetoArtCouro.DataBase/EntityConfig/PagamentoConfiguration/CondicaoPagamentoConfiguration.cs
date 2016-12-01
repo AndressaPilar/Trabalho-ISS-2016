e1e0a0568b529e47d85aa4b76cc521596cbcdbbc
TadaoHara <tadaohara@msn.com>
2016-12-01 20:56:14 -0200

    model e configuraçaõ
    Estoque, CondicaoPagto

---
+++ ProjetoArtCouro/ProjetoArtCouro.DataBase/EntityConfig/PagamentoConfiguration/CondicaoPagamentoConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.DataBase.EntityConfig.PagamentoConfiguration
+{
+    public class CondicaoPagamentoConfiguration : EntityTypeConfiguration<CondicaoPagamento>
+    {
+        public CondicaoPagamentoConfiguration()
+        {
+            ToTable("CondicaoPagamento");
+
+            Property(x => x.CondicaoPagamentoId)
+               .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(x => x.CondicaoPagamentoCodigo)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(x => x.Descricao)
+               .IsRequired()
+               .HasMaxLength(30)
+               .HasColumnType("varchar");
+
+            Property(x => x.Ativo)
+                .IsRequired()
+                .HasColumnType("bit");
+
+            Property(x => x.QuantidadeParcelas)
+                .IsRequired()
+                .HasColumnType("int");
+        }
+    }
+}
---
+++ ProjetoArtCouro/ProjetoArtCouro.Domain/Models/Estoques/Estoque.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Domain.Models.Estoques
+{
+    public class Estoque
+    {
+        public Guid EstoqueId { get; set; }
+        public int EstoqueCodigo { get; set; }
+        public DateTime DataUltimaEntrada { get; set; }
+        public int Quantidade { get; set; }
+        public virtual Produto Produto { get; set; }
+        public virtual Pessoa Fornecedor { get; set; }
+    }
+}
