714f9bdda6baafac8c9dc7250663b850cfead3bb
shDiego <dschiavinatti@gmail.com>
2016-12-04 20:35:48 -0200

    Configurando api - AutoMapper

---
+++ ProjetoArtCouro/ProjetoArtCouro.Api/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Api.AutoMapper
+{
+    public class AutoMapperConfig
+    {
+        public static void RegisterMappings()
+        {
+            //Definindo e Iniciando os mapeamentos
+            Mapper.Initialize(x =>
+            {
+                x.AddProfile<DomainToModelMappingProfile>();
+                x.AddProfile<ModelToDomainMappingProfile>();
+                x.AddProfile<ModelToModelMappingProfile>();
+            });
+        }
+    }
+}
