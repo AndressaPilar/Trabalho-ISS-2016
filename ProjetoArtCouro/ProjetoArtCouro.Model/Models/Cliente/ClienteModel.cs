15b49d24dc23d80c8aa79d769c300aebe01db242
GuGB <gustavo_gravena@hotmail.com>
2016-12-04 15:24:58 -0200

    Projeto Model
    Cleinte e contaReceber

---
+++ ProjetoArtCouro/ProjetoArtCouro.Model/Models/Cliente/ClienteModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Model.Models.Cliente
+{
+    public class ClienteModel : PessoaModel
+    {
+    }
+}
---
+++ ProjetoArtCouro/ProjetoArtCouro.Model/Models/Cliente/PesquisaClienteModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Model.Models.Cliente
+{
+    public class PesquisaClienteModel
+    {
+        [Display(Name = "Code", ResourceType = typeof(Mensagens))]
+        public int? CodigoCliente { get; set; }
+
+        [Display(Name = "Name", ResourceType = typeof(Mensagens))]
+        public string Nome { get; set; }
+
+        [Display(Name = "CPFCNPJ", ResourceType = typeof(Mensagens))]
+        public string CPFCNPJ { get; set; }
+
+        [Display(Name = "Email", ResourceType = typeof(Mensagens))]
+        public string Email { get; set; }
+
+        [Display(Name = "TypeOfPerson", ResourceType = typeof(Mensagens))]
+        public bool EPessoaFisica { get; set; }
+    }
+}
