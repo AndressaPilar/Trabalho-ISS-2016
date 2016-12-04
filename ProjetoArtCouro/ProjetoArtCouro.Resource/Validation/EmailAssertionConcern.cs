8fae17dea8d9d6aab990a0a0d639e232fddafde8
AndressaPilar <andressa.karla7@outlook.com>
2016-12-04 18:01:31 -0200

    Validation
    Email, Password e AssertionConcern

---
+++ ProjetoArtCouro/ProjetoArtCouro.Resource/Validation/EmailAssertionConcern.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Resource.Validation
+{
+    public class EmailAssertionConcern
+    {
+        public static void AssertIsValid(string email)
+        {
+            if (!Regex.IsMatch(email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase))
+                throw new Exception(Erros.InvalidEmail);
+        }
+    }
+}
---
+++ ProjetoArtCouro/ProjetoArtCouro.Resource/Validation/PasswordAssertionConcern.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.Resource.Validation
+{
+    public class PasswordAssertionConcern
+    {
+        public static void AssertIsValid(string password)
+        {
+            AssertionConcern.AssertArgumentNotNull(password, Erros.InvalidUserPassword);
+        }
+
+        public static string Encrypt(string password)
+        {
+            password += "|2d331cca-f6c0-40c0-bb43-6e32989c2881";
+            var md5 = MD5.Create();
+            var data = md5.ComputeHash(Encoding.Default.GetBytes(password));
+            var sbString = new StringBuilder();
+            foreach (var t in data)
+            {
+                sbString.Append(t.ToString("x2"));
+            }
+            return sbString.ToString();
+        }
+    }
+}
