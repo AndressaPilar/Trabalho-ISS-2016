534ca3dcc7994aa21832fe6744d6eec2a486fa68
shDiego <dschiavinatti@gmail.com>
2016-12-04 21:49:41 -0200

    Contrllers e Views Compra, funcionario e grupo

---
+++ ProjetoArtCouro/ProjetoArtCouro.Web/Controllers/Compras/CompraController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoArtCouro.Web.Controllers.Compras
+{
+    public class CompraController : BaseController
+    {
+        [CustomAuthorize(Roles = "PesquisaCompra")]
+        public ActionResult PesquisaCompra()
+        {
+            ViewBag.Title = Mensagens.Buy;
+            ViewBag.SubTitle = Mensagens.SearchBuy;
+            ViewBag.StatusCompra = new List<LookupModel>
+            {
+                new LookupModel
+                {
+                    Codigo = 1,
+                    Nome = "Aberto"
+                },
+                new LookupModel
+                {
+                    Codigo = 2,
+                    Nome = "Confirmado"
+                },
+                new LookupModel
+                {
+                    Codigo = 3,
+                    Nome = "Cancelado"
+                }
+            };
+            return View();
+        }
+
+        [HttpPost]
+        [CustomAuthorize(Roles = "PesquisaCompra")]
+        public JsonResult PesquisaCompra(PesquisaCompraModel model)
+        {
+            var response = ServiceRequest.Post<List<CompraModel>>(model, "api/Compra/PesquisarCompra");
+            if (response.Data.ObjetoRetorno != null && !response.Data.ObjetoRetorno.Any())
+            {
+                response.Data.Mensagem = Erros.NoShoppingForTheGivenFilter;
+            }
+            return ReturnResponse(response);
+        }
+
+        [CustomAuthorize(Roles = "NovaCompra")]
+        public ActionResult NovaCompra()
+        {
+            ViewBag.Title = Mensagens.Buy;
+            ViewBag.SubTitle = Mensagens.NewBuy;
+            ViewBag.Produtos = new List<LookupModel>();
+            var model = new CompraModel
+            {
+                StatusCompra = "Aberto",
+                DataCadastro = string.Format("{0:dd/MM/yyyy H:mm}", DateTime.Now),
+                ValorTotalBruto = "0,00",
+                ValorTotalLiquido = "0,00",
+                ValorTotalFrete = "0,00"
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [CustomAuthorize(Roles = "NovaCompra")]
+        public JsonResult NovaCompra(CompraModel model)
+        {
+            var response = ServiceRequest.Post<RetornoBase<object>>(model, "api/Compra/CriarCompra");
+            return ReturnResponse(response);
+        }
+
+        [CustomAuthorize(Roles = "EditarCompra")]
+        public ActionResult EditarCompra(int codigoCompra)
+        {
+            ViewBag.Title = Mensagens.Buy;
+            ViewBag.SubTitle = Mensagens.EditBuy;
+            ViewBag.Produtos = new List<LookupModel>();
+            var response = ServiceRequest.Post<CompraModel>(new { codigoCompra = codigoCompra }, "api/Compra/PesquisarCompraPorCodigo");
+            var model = response.Data.ObjetoRetorno;
+            return View(model);
+        }
+
+        [HttpPost]
+        [CustomAuthorize(Roles = "EditarCompra")]
+        public ActionResult EditarCompra(CompraModel model)
+        {
+            var response = ServiceRequest.Put<RetornoBase<object>>(model, "api/Compra/EditarCompra");
+            return ReturnResponse(response);
+        }
+
+        [HttpPost]
+        [CustomAuthorize(Roles = "ExcluirCompra")]
+        public JsonResult ExcluirCompra(int codigoCompra)
+        {
+            var response = ServiceRequest.Delete<RetornoBase<object>>(new { codigoCompra = codigoCompra }, "api/Compra/ExcluirCompra");
+            return ReturnResponse(response);
+        }
+
+        [CustomAuthorize(Roles = "NovaCompra")]
+        public JsonResult ObterListaFornecedor()
+        {
+            var response = ServiceRequest.Get<List<FornecedorModel>>("api/Fornecedor/ObterListaFornecedor");
+            return ReturnResponse(response);
+        }
+
+        [CustomAuthorize(Roles = "NovaCompra")]
+        public JsonResult ObterListaFormasPagamento()
+        {
+            var response = ServiceRequest.Get<List<FormaPagamentoModel>>("api/FormaPagamento/ObterListaFormaPagamento");
+            return ReturnResponse(response);
+        }
+
+        [CustomAuthorize(Roles = "NovaCompra")]
+        public JsonResult ObterListaCondicoesPagamento()
+        {
+            var response = ServiceRequest.Get<List<CondicaoPagamentoModel>>("api/CondicaoPagamento/ObterListaCondicaoPagamento");
+            return ReturnResponse(response);
+        }
+
+        [CustomAuthorize(Roles = "NovaCompra")]
+        public JsonResult ObterListaProduto()
+        {
+            var response = ServiceRequest.Get<List<ProdutoModel>>("api/Produto/ObterListaProduto");
+            return ReturnResponse(response);
+        }
+    }
+}
---
+++ ProjetoArtCouro/ProjetoArtCouro.Web/Controllers/Pessoas/FuncionarioController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoArtCouro.Web.Controllers.Pessoas
+{
+    public class FuncionarioController : BaseController
+    {
+        [CustomAuthorize(Roles = "PesquisaFuncionario")]
+        public ActionResult PesquisaFuncionario()
+        {
+            ViewBag.Title = Mensagens.Employee;
+            ViewBag.SubTitle = Mensagens.SearchEmployee;
+            return View();
+        }
+
+        [HttpPost]
+        [CustomAuthorize(Roles = "PesquisaFuncionario")]
+        public JsonResult PesquisaFuncionario(PesquisaFuncionarioModel model)
+        {
+            var response = ServiceRequest.Post<List<FuncionarioModel>>(model, "api/Funcionario/PesquisarFuncionario");
+            if (response.Data.ObjetoRetorno != null && !response.Data.ObjetoRetorno.Any())
+            {
+                response.Data.Mensagem = Erros.NoEmployeeForTheGivenFilter;
+            }
+            return ReturnResponse(response);
+        }
+
+        [CustomAuthorize(Roles = "NovoFuncionario")]
+        public ActionResult NovoFuncionario()
+        {
+            CriarViewBags(Mensagens.NewEmployee);
+            var listaBase = new List<LookupModel>();
+            ViewBag.Enderecos = listaBase;
+            ViewBag.Telefones = listaBase;
+            ViewBag.Celulares = listaBase;
+            ViewBag.Emails = listaBase;
+            return View();
+        }
+
+        [HttpPost]
+        [CustomAuthorize(Roles = "NovoFuncionario")]
+        public JsonResult NovoFuncionario(FuncionarioModel model)
+        {
+            model.PapelPessoa = (int)TipoPapelPessoaEnum.Funcionario;
+            var response = ServiceRequest.Post<RetornoBase<object>>(model, "api/Funcionario/CriarFuncionario");
+            return ReturnResponse(response);
+        }
+
+        [CustomAuthorize(Roles = "EditarFuncionario")]
+        public ActionResult EditarFuncionario(int codigoFuncionario)
+        {
+            CriarViewBags(Mensagens.EditEmployee);
+            var response = ServiceRequest.Post<FuncionarioModel>(new { codigoFuncionario = codigoFuncionario }, "api/Funcionario/PesquisarFuncionarioPorCodigo");
+            var model = response.Data.ObjetoRetorno;
+            if (model == null)
+            {
+                return View(response.Data.ObjetoRetorno);
+            }
+
+            var listaBase = new List<LookupModel>();
+            ViewBag.Enderecos = model.Enderecos.Select(x => new LookupModel
+            {
+                Codigo = x.EnderecoId ?? 0,
+                Nome = string.Format("Logradouro: {0}, N: {1}, Cidade: {2}, CEP: {3}", x.Logradouro, x.Numero, x.Cidade, x.Cep)
+            }).ToList();
+            ViewBag.Telefones = model.MeioComunicacao.Telefones ?? listaBase;
+            ViewBag.Celulares = model.MeioComunicacao.Celulares ?? listaBase;
+            ViewBag.Emails = model.MeioComunicacao.Emalis ?? listaBase;
+
+            return View(response.Data.ObjetoRetorno);
+        }
+
+        [HttpPost]
+        [CustomAuthorize(Roles = "EditarFuncionario")]
+        public JsonResult EditarFuncionario(FuncionarioModel model)
+        {
+            var response = ServiceRequest.Put<RetornoBase<object>>(model, "api/Funcionario/EditarFuncionario");
+            return ReturnResponse(response);
+        }
+
+        [HttpPost]
+        [CustomAuthorize(Roles = "ExcluirFuncionario")]
+        public JsonResult ExcluirFuncionario(int codigoFuncionario)
+        {
+            var response = ServiceRequest.Delete<RetornoBase<object>>(new { codigoFuncionario = codigoFuncionario }, "api/Funcionario/ExcluirFuncionario");
+            return ReturnResponse(response);
+        }
+
+        private void CriarViewBags(string subTitle)
+        {
+            ViewBag.Title = Mensagens.Employee;
+            ViewBag.SubTitle = subTitle;
+
+            var listaBase = new List<LookupModel>();
+            var response = ServiceRequest.Get<List<LookupModel>>("api/Pessoa/ObterListaEstado");
+            if (response.Data.TemErros)
+            {
+                ModelState.AddModelError("Erro", response.Data.Mensagem);
+                ViewBag.Estados = listaBase;
+            }
+            ViewBag.Estados = response.Data.ObjetoRetorno;
+            response = ServiceRequest.Get<List<LookupModel>>("api/Pessoa/ObterListaEstadoCivil");
+            if (response.Data.TemErros)
+            {
+                ModelState.AddModelError("Erro", response.Data.Mensagem);
+                ViewBag.EstadosCivis = listaBase;
+            }
+            ViewBag.EstadosCivis = response.Data.ObjetoRetorno;
+        }
+    }
+}
