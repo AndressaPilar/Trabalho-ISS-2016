cc8eae0b5cd820f717aa115f4446c1da0160425f
TadaoHara <tadaohara@msn.com>
2016-12-04 23:23:40 -0200

    Itemvenda configuration e repository e pael repository

---
+++ ProjetoArtCouro/ProjetoArtCouro.DataBase/EntityConfig/VendaConfiguration/ItemVendaConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoArtCouro.DataBase.EntityConfig.VendaConfiguration
+{
+    public class ItemVendaConfiguration : EntityTypeConfiguration<ItemVenda>
+    {
+        public ItemVendaConfiguration()
+        {
+            ToTable("ItemVenda");
+
+            Property(x => x.ItemVendaId)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(x => x.ItemVendaCodigo)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(x => x.ProdutoCodigo)
+                .IsRequired()
+                .HasColumnType("int");
+
+            Property(x => x.ProdutoNome)
+                .IsRequired()
+                .HasColumnType("varchar");
+
+            Property(x => x.Quantidade)
+                .IsRequired()
+                .HasColumnType("int");
+
+            Property(x => x.PrecoVenda)
+                .IsRequired()
+                .HasColumnType("decimal");
+
+            Property(x => x.ValorBruto)
+                .IsRequired()
+                .HasColumnType("decimal");
+
+            Property(x => x.ValorDesconto)
+                .HasColumnType("decimal");
+
+            Property(x => x.ValorLiquido)
+                .IsRequired()
+                .HasColumnType("decimal");
+        }
+    }
+}
