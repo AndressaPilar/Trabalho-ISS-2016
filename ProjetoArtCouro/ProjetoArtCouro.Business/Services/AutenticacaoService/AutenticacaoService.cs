310eb1f0b3ab05aaeeb7a16084b6f262bc909b7c
AndressaPilar <andressa.karla7@outlook.com>
2017-01-14 17:23:53 -0200

    ProjetoArtCouro.Business: AutenticacaoService e CompraService
    No ProjetoArtCouro.Business, nas pastas AutenticacaoService e
    CompraService foi criado as classes AutenticacaoService.cs e
    ContaPagarService.cs que estavam faltando da minha parte

---
+++ ProjetoArtCouro/ProjetoArtCouro.Business/Services/AutenticacaoService/AutenticacaoService.cs
@@ -0,0 +1,31 @@
+﻿using ProjetoArtCouro.Domain.Contracts.IRepository.IUsuario;
+using ProjetoArtCouro.Domain.Contracts.IService.IAutenticacao;
+using ProjetoArtCouro.Domain.Models.Usuarios;
+using ProjetoArtCouro.Resource.Validation;
+
+namespace ProjetoArtCouro.Business.Services.AutenticacaoService
+{
+    public class AutenticacaoService : IAutenticacao
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+        public AutenticacaoService(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        public Usuario AutenticarUsuario(string usuarioNome, string senha)
+        {
+            var usuario = _usuarioRepository.ObterComPermissoesPorUsuarioNome(usuarioNome);
+            if (usuario == null || !PasswordAssertionConcern.Encrypt(senha).Equals(usuario.Senha))
+            {
+                return null;
+            }
+            return usuario;
+        }
+
+        public void Dispose()
+        {
+            _usuarioRepository.Dispose();
+        }
+    }
+}
