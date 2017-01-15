16e4988ad5cf155880293ec79e1ecd5f58b929c4
shDiego <dschiavinatti@gmail.com>
2017-01-15 15:38:08 -0200

    ProjetoArtCouro.Model: Models > Common e arquivo XML de Configuração
    Na camada ProjetoArtCouro.Model, nas pastas Models > Common, foi criado
    as classes LookupModel.cs; RetornoBase.cs, que estavam faltando da minha
    parte.
    Foi criado também 1 arquivo XML de Configuração packages.config

---
+++ ProjetoArtCouro/ProjetoArtCouro.Model/Models/Common/RetornoBase.cs
@@ -0,0 +1,16 @@
+﻿using ProjetoArtCouro.Resource.Resources;
+
+namespace ProjetoArtCouro.Model.Models.Common
+{
+    public class RetornoBase<T>
+    {
+        public RetornoBase()
+        {
+            Mensagem = Mensagens.ReturnSuccess;
+        }
+
+        public string Mensagem { get; set; }
+        public bool TemErros { get; set; }
+        public T ObjetoRetorno { get; set; }
+    }
+}
