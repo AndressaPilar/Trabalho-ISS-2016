a709701d35df5a92ebff0c5d281c243fec70e143
shDiego <dschiavinatti@gmail.com>
2017-01-19 22:12:23 -0200

    Divisão entre 3 integrantes - Parte I -> ProjetoArtCouro.Test: TesteApiContaReceber > TestContaReceberController.cs
    -   Devido saída de 1 integrante da nossa equipe, esse teste teve que
    ser dividido entre os integrantes restantes
    - Na camada ProjetoArtCouro.Test, na pasta TesteApiContaReceber foi
    criado os dois primeiros testes unitários na classe
    TestContaReceberController.cs:
    -  [TestClass] TestContaReceberController;
    -  [TestInitialize] AutoMapper();

---
+++ ProjetoArtCouro/Projeto.ArtCouro.Test/TesteApiContaReceber/TestContaReceberController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ProjetoArtCouro.Api.Controllers.ContasReceber;
+using ProjetoArtCouro.Business.Services.VendaService;
+using ProjetoArtCouro.Domain.Contracts.IRepository.IVenda;
+using ProjetoArtCouro.Domain.Contracts.IService.IVenda;
+using ProjetoArtCouro.Domain.Models.Enums;
+using ProjetoArtCouro.Domain.Models.Vendas;
+using ProjetoArtCouro.Model.Models.Common;
+using ProjetoArtCouro.Model.Models.ContaReceber;
+using ProjetoArtCouro.Resource.Resources;
+
+namespace ProjetoArtCouro.Test.TesteApiContaReceber
+{
+    [TestClass]
+    public class TestContaReceberController
+    {
+        private readonly Mock<IContaReceberRepository> _mockContaReceberRepository = new Mock<IContaReceberRepository>();
+
+        private static ContaReceberController CreateContaReceberController(IMock<IContaReceberRepository> mockContaReceberRepository)
+        {
+            var contaReceberService = new ContaReceberService(mockContaReceberRepository.Object);
+            var controller = new ContaReceberController(contaReceberService)
+            {
+                Request = new HttpRequestMessage(),
+                Configuration = new HttpConfiguration()
+            };
+            return controller;
+        }
+
+        [TestInitialize]
+        public void AutoMapper()
+        {
+            Mapper.CreateMap<ContaReceberModel, ContaReceber>()
+                .ForMember(d => d.ContaReceberCodigo, m => m.MapFrom(s => s.CodigoContaReceber))
+                .ForMember(d => d.DataVencimento, m => m.MapFrom(s => s.DataVencimento))
+                .ForMember(d => d.Recebido, m => m.MapFrom(s => s.Recebido))
+                .ForMember(d => d.StatusContaReceber,
+                    m => m.MapFrom(s => Enum.Parse(typeof(StatusContaReceberEnum), s.Status)))
+                .ForMember(d => d.ValorDocumento, m => m.MapFrom(s => s.ValorDocumento));
+
+            Mapper.CreateMap<ContaReceber, ContaReceberModel>()
+                .ForMember(d => d.CodigoVenda, m => m.MapFrom(s => s.Venda.VendaCodigo))
+                .ForMember(d => d.CodigoCliente, m => m.MapFrom(s => s.Venda.Cliente.PessoaCodigo))
+                .ForMember(d => d.DataEmissao, m => m.MapFrom(s => s.Venda.DataCadastro))
+                .ForMember(d => d.DataVencimento, m => m.MapFrom(s => s.DataVencimento))
+                .ForMember(d => d.ValorDocumento, m => m.MapFrom(s => s.ValorDocumento))
+                .ForMember(d => d.Status, m => m.MapFrom(s => s.StatusContaReceber.ToString()))
+                .ForMember(d => d.StatusId, m => m.MapFrom(s => (int)s.StatusContaReceber))
+                .ForMember(d => d.Recebido, m => m.MapFrom(s => s.Recebido))
+                .ForMember(d => d.NomeCliente, m => m.MapFrom(s => s.Venda.Cliente.Nome))
+                .AfterMap((s, d) =>
+                {
+                    d.CPFCNPJ = s.Venda.Cliente.PessoaFisica != null
+                        ? s.Venda.Cliente.PessoaFisica.CPF
+                        : s.Venda.Cliente.PessoaJuridica.CNPJ;
+                });
+        }
+    }
+}
